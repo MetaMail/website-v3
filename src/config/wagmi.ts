ecb512d2ab2f585b2534f05cf51d71528fa2cf19
suneal <xurigong@gmail.com>
2026-02-17 09:00:22 +0800

    update

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -7,8 +7,7 @@ import { getDefaultConfig } from "connectkit";
 export const wagmiConfig = createConfig(
   getDefaultConfig({
     appName: "MetaMail",
-    walletConnectProjectId:
-      process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "demo",
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "d76ba7c7d72830d1ef0f73610b9e9ed3",
     chains: [mainnet],
     enableFamily: false,
   })
